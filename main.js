94a267194e304075c0c3abf0196e5c11ec27c777
Felipe Mello <86944431+felipe-mello2@users.noreply.github.com>
2024-03-05 02:43:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { Player } = require('discord-player');
 const Genius = require("genius-lyrics");
 const { Client, GatewayIntentBits } = require('discord.js');
+const ytdl = require('ytdl-core');
 const keep_alive = require('./keep_alive.js')
 
 global.client = new Client({
@@ -16,10 +17,28 @@ global.client = new Client({
 
 client.config = require('./config');
 
+// Set YTDL_NO_UPDATE environment variable
+process.env.YTDL_NO_UPDATE = true;
+
 const player = new Player(client, client.config.opt.discordPlayer);
 global.genius = new Genius.Client();
 player.extractors.loadDefault();
 
+async function downloadVideo(videoURL) {
+    try {
+        const info = await ytdl.getInfo(videoURL);
+        const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+        // Rest of your code for downloading the video
+    } catch (error) {
+        if (error.message === 'ERR_NO_RESULT') {
+            console.error('Could not extract stream for this track');
+            // Handle this specific error case
+        } else {
+            console.error('An error occurred:', error.message);
+            // Handle other errors
+        }
+    }
+}
 
 require('./src/loader');
 
